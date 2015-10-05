9853e2b44ca88915ca35ad58851764e6010278a3
monaLeti <you@example.com>
2015-10-05 16:20:54 +0200

    Team page

--- menu.js
+++ menu.js
@@ -13,7 +13,6 @@ c.onmouseover = function (){
   l.style.marginTop="15%";
   te.style.marginTop="15%";
   textHow.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. All of this data is collected locally on your private smart device, and is therefore only shared with you";
-
 }
 c.onmouseout = function(){
 
