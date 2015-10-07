04d7a39fd0bde98bf18c6b7cc5ac6d5ee39e2a38
monaLeti <you@example.com>
2015-10-07 16:05:51 +0200

    One menu for all

--- menu.js
+++ menu.js
@@ -5,48 +5,45 @@ var te = document.getElementById('team');
 var textHow =document.getElementById('howWorksExplanation');
 var textNeed =document.getElementById('needExplanation');
 var textNew =document.getElementById('newsExplanation');
+var textTeam = document.getElementById('teamExplanation');
 //HowWors hover --> Everything gets compacted
-c.onmouseover = function (){
+if(c!=null){
+  c.onmouseover = function (){
+    c.style.marginBottom="5%"; //Distance between the text and the botton
+    textHow.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. All of this data is collected locally on your private smart device, and is therefore only shared with you";
+  }
 
-  c.style.marginBottom="5%"; //Distance between the text and the botton
-  n.style.marginTop="15%";
-  l.style.marginTop="15%";
-  te.style.marginTop="15%";
-  textHow.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. All of this data is collected locally on your private smart device, and is therefore only shared with you";
+  c.onmouseout = function(){
+    c.style.marginBottom="0%";
+    textHow.innerHTML="";
+  }
 }
-c.onmouseout = function(){
-
-  c.style.marginBottom="0%";
-  n.style.marginTop="10%";
-  l.style.marginTop="10%";
-  te.style.marginTop="10%";
-  textHow.innerHTML="";
+if(n!=null){
+  n.onmouseover = function (){
+    n.style.marginBottom="5%";
+    textNeed.innerHTML = "NoviPel are always looking for co-operators. Are you a professional practitioner, investor or maybe just interested in getting toknow us better; dont hesistate to get in contact today!";
+  }
+  n.onmouseout = function(){
+    n.style.marginBottom="0%";
+    textNeed.innerHTML="";
+  }
 }
-
-n.onmouseover = function (){
-  n.style.marginBottom="5%";
-  textNeed.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. All of this data is collected locally on your private smart device, and is therefore only shared with you";
-
+if(n!=null){
+  l.onmouseover = function (){
+    l.style.marginBottom="5%";
+    textNew.innerHTML = "Check up on the current status and workshops of NoviPel here.<br>Read our previous and current newsletters";
+  }
+  l.onmouseout = function(){
+    l.style.marginBottom="0%";
+    textNew.innerHTML ="";
+  }
 }
-n.onmouseout = function(){
-  n.style.marginBottom="0%";
-  textNeed.innerHTML="";
-}
-
-
-l.onmouseover = function (){
-  l.style.marginBottom="5%";
-  textNew.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. All of this data is collected locally on your private smart device, and is therefore only shared with you";
-}
-l.onmouseout = function(){
-  l.style.marginBottom="0%";
-  textNew.innerHTML ="";
-}
-
-
-te.onmouseover = function (){
-  te.style.marginBottom="50%";
-}
-te.onmouseout = function(){
-  te.style.marginBottom="0%";
+if(te!=null){
+  te.onmouseover = function (){
+    te.style.marginBottom="5%";
+    textTeam.innerHTML ="The team behind NoviPel."
+  }
+  te.onmouseout = function(){
+    textTeam.innerHTML ="";
+  }
 }
