dafa86607568227afb57096dd05cd3d166a6d599
monaLeti <you@example.com>
2015-10-09 12:41:54 +0200

    One common menu and bootstrap

--- menu.js
+++ menu.js
@@ -9,38 +9,37 @@ var textTeam = document.getElementById('teamExplanation');
 //HowWors hover --> Everything gets compacted
 if(c!=null){
   c.onmouseover = function (){
-    c.style.marginBottom="5%"; //Distance between the text and the botton
     textHow.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. All of this data is collected locally on your private smart device, and is therefore only shared with you";
+    textHow.style.marginBottom="5%";
   }
 
   c.onmouseout = function(){
-    c.style.marginBottom="0%";
+    textHow.style.marginBottom="0%";
     textHow.innerHTML="";
   }
 }
 if(n!=null){
   n.onmouseover = function (){
-    n.style.marginBottom="5%";
     textNeed.innerHTML = "NoviPel are always looking for co-operators. Are you a professional practitioner, investor or maybe just interested in getting toknow us better; dont hesistate to get in contact today!";
+    textNeed.style.marginBottom="5%";
   }
   n.onmouseout = function(){
-    n.style.marginBottom="0%";
+    textNeed.style.marginBottom="0%";
     textNeed.innerHTML="";
   }
 }
 if(n!=null){
   l.onmouseover = function (){
-    l.style.marginBottom="5%";
     textNew.innerHTML = "Check up on the current status and workshops of NoviPel here.<br>Read our previous and current newsletters";
+    textNew.style.marginBottom="5%";
   }
   l.onmouseout = function(){
-    l.style.marginBottom="0%";
+    textNew.style.marginBottom="0%";
     textNew.innerHTML ="";
   }
 }
 if(te!=null){
   te.onmouseover = function (){
-    te.style.marginBottom="5%";
     textTeam.innerHTML ="The team behind NoviPel."
   }
   te.onmouseout = function(){
---
+++ team.js~
@@ -0,0 +1,11 @@
+angular.module('teamApp', ['ngAnimate'])
+  .controller('teamController', function(){
+    var teamList = this;
+    teamList.list = [{people: [{name: 'Alexander', img1: '../images/a.png', img2: '../images/dibujo1.png',toggle:true},
+                                {name: 'Christian', img1: '../images/christian.jpg', img2: '../images/dibujo.png',toggle:true},
+                              {name: 'Louis', img1: '../images/louise.jpg', img2: '../images/dibujo2.png',toggle:true}]},
+                    {people:[{name:'Jakob', img1:'../images/j.png', img2: '../images/dibujo3.png',toggle:true},
+                              {name: 'Noam', img1:'../images/n.png', img2: '../images/dibujo4.png',toggle:true},
+                            {name: 'Leticia', img1: '../images/l.png', img2: '../images/dibujo5.png',toggle:true}]}
+                    ];
+  });
