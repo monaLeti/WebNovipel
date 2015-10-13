03d28a8ca5cd9ac55b9bfbbd2caddf13b3d390f7
monaLeti <you@example.com>
2015-10-13 13:25:47 +0200

    version to launch

--- menu.js
+++ menu.js
@@ -9,8 +9,9 @@ var textTeam = document.getElementById('teamExplanation');
 //HowWors hover --> Everything gets compacted
 if(c!=null){
   c.onmouseover = function (){
-    textHow.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. All of this data is collected locally on your private smart device, and is therefore only shared with you";
+    textHow.innerHTML = "The device, which is made up of body friendly materials, is inserted into the vagina. With the specially designed NoviPel sensorsystem, all muscle activity is monitored and analyzed wirelessly. \nThe data is then autonomously verified as correct or incorrect. This enables the user to be able to train and convey the pelvic floor muscles in a novel way. <br>All of this data is collected locally on your private smart device, and is therefore only shared with you";
     textHow.style.marginBottom="5%";
+
   }
 
   c.onmouseout = function(){
@@ -28,7 +29,7 @@ if(n!=null){
     textNeed.innerHTML="";
   }
 }
-if(n!=null){
+if(l!=null){
   l.onmouseover = function (){
     textNew.innerHTML = "Check up on the current status and workshops of NoviPel here.<br>Read our previous and current newsletters";
     textNew.style.marginBottom="5%";
